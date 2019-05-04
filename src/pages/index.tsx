6587fe89582d3d1e9f2e6d2aeb7dafddb278e6ea
Charlotte Fereday <cfereday@thoughtworks.com>
2019-05-04 23:38:45 +0100

    Charlotte/felipe Adds initial styling for index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,55 @@
 import * as React from 'react'
 
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+import { colors } from '../styles/variables'
 
 const IndexPage = () => (
-  <>
+  <Main>
+    <GlobalStyle />
     <Title>Hi people</Title>
-    <Column>
-      <img src="https://themes.gohugo.io/theme/hallo-hugo/images/portrait.jpg" className="portrait" alt="Portrait" />
+    <Column left>
+      <Portrait src="https://themes.gohugo.io/theme/hallo-hugo/images/portrait.jpg" alt="Portrait" />
     </Column>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </>
+    <Column>
+      <Font>Welcome to your new Gatsby site.</Font>
+    </Column>
+  </Main>
 )
+
+const Portrait = styled.img`
+  box-sizing: border-box;
+  border-radius: 50%;
+  border: 10px solid #fff;
+  margin: 2em 3em;
+  width: 100%;
+  height: auto;
+  max-width: 300px;
+  max-height: 300px;
+`
+
+const GlobalStyle = createGlobalStyle`
+  body { 
+  background-color: ${colors.nordic.teal};
+  }
+`
+
+const Main = styled.main`
+  display: flex;
+  margin-top: 20vh;
+`
 
 const Column = styled.div`
   flex: 1;
-  text-align: end;
+  text-align: ${props => (props.left ? 'end' : 'start')};
 `
 
 const Title = styled.h1`
-  color: red;
+  color: ${colors.nordic.darkBlue};
+`
+
+const Font = styled.p`
+  font-family: Montserrat, sans-serif;
+  flex-wrap: wrap;
 `
 
 export default IndexPage
