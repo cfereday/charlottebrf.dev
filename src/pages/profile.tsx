9ed1ec9ec40b95a9d205fb428bd869104b31ddba
Charlotte Fereday <cfereday@thoughtworks.com>
2019-07-10 17:56:41 +0200

    Charlotte - refactors Profile component to have a resuable experienceItem to reduce repeated code

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -8,6 +8,30 @@ import { NightParagraph } from '../components/shared/Paragraph'
 import { NightHeader3, NightHeader4 } from '../components/shared/Header'
 import { Links } from '../components/shared/Links'
 
+interface ParagraphText {
+  header: string
+  techStack?: string
+  skills: string
+}
+
+const experienceItem = (text: ParagraphText) => {
+  return (
+    <div>
+      <NightHeader3>{text.header}</NightHeader3>
+      <NightParagraph>
+        <ul>
+          <li>
+            <b>Tech stack:</b> {text.techStack}
+          </li>
+          <li>
+            <b>Skills:</b> {text.skills}
+          </li>
+        </ul>
+      </NightParagraph>
+    </div>
+  )
+}
+
 const profile = () => (
   <Base>
     <Column right>
@@ -30,50 +54,26 @@ const profile = () => (
           <NightParagraph>
             Consulting as an engineer on a variety of different projects. Pairing, TDD, Agile methodologies & much more.
           </NightParagraph>
-          <NightHeader4> International FinTech Startup</NightHeader4>
-          <NightParagraph>
-            <ul>
-              <li>
-                <b>Tech stack:</b> TypeScript, React, React Hooks & Context API, Apollo Server, GraphQL, Java
-              </li>
-              <li>
-                <b>Skills:</b> Coaching Agile best practices, Greenfield project
-              </li>
-            </ul>
-          </NightParagraph>
-          <NightHeader4> Public Sector Client</NightHeader4>
-          <NightParagraph>
-            <ul>
-              <li>
-                <b>Tech stack:</b> Clojure, Clara Rules, MongoDB, RabbitMQ, ELK stack, Vagrant
-              </li>
-              <li>
-                <b>Skills:</b> Agile Delivery, Microservices architecture
-              </li>
-            </ul>
-          </NightParagraph>
-          <NightHeader4>Startup Challenger Bank</NightHeader4>
-          <NightParagraph>
-            <ul>
-              <li>
-                <b>Tech stack:</b> Clojure, JavaScript, React Native, Redux, Sagas, AWS
-              </li>
-              <li>
-                <b>Skills:</b> Product market fit, Greenfield project
-              </li>
-            </ul>
-          </NightParagraph>
-          <NightHeader4>ThoughtWorks University Training scheme</NightHeader4>
-          <NightParagraph>
-            <ul>
-              <li>
-                <b>Tech stack:</b> Java, JavaScript, PostgreSQL
-              </li>
-              <li>
-                <b>Skills:</b> Agile processes, Automation Testing, CFR's testing
-              </li>
-            </ul>
-          </NightParagraph>
+          {experienceItem({
+            header: 'International FinTech Startup',
+            techStack: 'TypeScript, React, React Hooks & Context API, Apollo Server, GraphQL, Java',
+            skills: 'Coaching Agile best practices, Greenfield project'
+          })}
+          {experienceItem({
+            header: 'Public Sector Client',
+            techStack: 'Clojure, Clara Rules, MongoDB, RabbitMQ, ELK stack, Vagrant',
+            skills: 'Agile Delivery, Microservices architecture'
+          })}
+          {experienceItem({
+            header: 'Startup Challenger Bank',
+            techStack: 'Clojure, JavaScript, React Native, Redux, Sagas, AWS',
+            skills: 'Product market fit, Greenfield project'
+          })}
+          {experienceItem({
+            header: 'ThoughtWorks University Training scheme',
+            techStack: 'Java, JavaScript, PostgreSQL',
+            skills: 'Agile processes, Automation Testing, CFR testing'
+          })}
         </TimelineItem>
         <TimelineItem
           key="002"
@@ -83,15 +83,11 @@ const profile = () => (
             color: `${NordicPalette.polarNight.charcoal}`
           }}
         >
-          <NightHeader3>Women in Tech Scholar, Makers Academy/ThoughtWorks</NightHeader3>
-          <NightParagraph>
-            <ul>
-              <li>One of a total of five scholars to receive support to retrain</li>
-              <li>
-                <b>Tech Stack:</b> Ruby, JavaScript, SQL
-              </li>
-            </ul>
-          </NightParagraph>
+          {experienceItem({
+            header: 'Women in Tech Scholar, Makers Academy/ThoughtWorks',
+            techStack: 'Ruby, JavaScript, SQL',
+            skills: 'One of a total of five scholars to receive support to retrain'
+          })}
         </TimelineItem>
         <TimelineItem
           key="003"
@@ -104,14 +100,11 @@ const profile = () => (
             boxShadow: '0.5rem 0.5rem 2rem 0 rgba(0, 0, 0, 0.2)'
           }}
         >
-          <NightHeader3>Programmes Manager, Code First: Girls</NightHeader3>
-          <NightParagraph>
-            <ul>
-              <li>
-                <b>Skills:</b> Stakeholder management, business development, community building
-              </li>
-            </ul>
-          </NightParagraph>
+          {experienceItem({
+            header: 'Programmes Manager, Code First: Girls',
+            techStack: 'Collaborated on creating beginner curriculum for: Ruby, Beginners Intro to Web Development',
+            skills: 'Stakeholder management, business development, community building'
+          })}
         </TimelineItem>
         <TimelineItem
           key="004"
@@ -121,14 +114,11 @@ const profile = () => (
             color: `${NordicPalette.polarNight.charcoal}`
           }}
         >
-          <NightHeader3>Knowledge Exchange Associate: Arts & Digital, King's Cultural Institute</NightHeader3>
-          <NightParagraph>
-            <ul>
-              <li>
-                <b>Skills:</b> Stakeholder management, innovation, cross-displine collaboration
-              </li>
-            </ul>
-          </NightParagraph>
+          {experienceItem({
+            header: 'Knowledge Exchange Associate: Arts & Digital, Kings Cultural Institute',
+            techStack: 'Overseeing management of a variety of web apps',
+            skills: 'Stakeholder management, innovation, cross-displine collaboration'
+          })}
         </TimelineItem>
         <TimelineItem
           key="005"
@@ -148,7 +138,10 @@ const profile = () => (
             <ul>
               <li>
                 AHRC funded PhD analysing memory, selfhood & exile. Read more about the PhD and research outputs
-                <Links href="https://kcl.academia.edu/CharlotteFereday" title="Research"> here</Links>
+                <Links href="https://kcl.academia.edu/CharlotteFereday" title="Research">
+                  {' '}
+                  here
+                </Links>
               </li>
             </ul>
             <NightHeader4>MA completed 09/2010</NightHeader4>
