b07ca330c5417cd9ee6ac3f67715d54b6e471e76
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-04-12 23:39:31 +0200

    Refactors community data to be separate from component

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -3,6 +3,11 @@ import charlotteWebsite from '../media/projects/CharloteWebsite.jpg'
 import reduceRewrite from '../media/projects/ReduceRewrite.jpg'
 import flatChore from '../media/projects/FlatChores.jpg'
 import coderNewbie from '../media/projects/CoderNewbie.jpg'
+import Ada from '../media/events/ada.png'
+import SoC from '../media/events/SoC.jpg'
+import Language from '../media/events/language-acts.png'
+import Glasgow from '../media/events/Glasgow.jpeg'
+import Eqtech from '../media/events/Eqtech.jpeg'
 
 export const nodeProject = {
   paragraph: 'Built using Javascript, Node, Postgres, Bcrypt, JWT ',
@@ -37,4 +42,44 @@ export const newbie = {
   url: 'https://github.com/charlottebrf/coder-newbie-advice',
   title: 'Coder Newbie',
   image: coderNewbie
+}
+
+export const adaLab = {
+  title: 'Adalab',
+  paragraph: 'Mentor for students at this free bootcamp to get more women in tech',
+  date: '2019',
+  url: 'https://adalab.es/hazte-voluntario/',
+  photo: Ada
+}
+
+export const soC = {
+  title: 'School of Code Bootcamp',
+  paragraph: 'Mentor for students at this free bootcamp for underrepresented groups in tech',
+  date: '2018 - 2019',
+  url: 'https://schoolofcode.co.uk',
+  photo: SoC
+}
+
+export const ideating = {
+  title: 'Ideating the Modern Language Curriculum',
+  paragraph: 'Organiser of a collaboration with educators on impact of tech for ML learning',
+  date: '15.06.19',
+  url: 'https://languageacts.org',
+  photo: Language
+}
+
+export const dojo = {
+  title: 'Glasgow Clojure Dojo',
+  paragraph: 'Organiser of meetup bringing together all levels to improve Clojure skills dojos',
+  date: '2018 - 2019',
+  url: 'https://www.meetup.com/Glasgow-Software-Development-Meetup/',
+  photo: Glasgow
+}
+
+export const eqTech = {
+  title: 'Ideating an Equitable Tech Future',
+  paragraph: 'Organiser of a day event to explore using tech for good with charities & third sector',
+  date: '19.09.17',
+  url: 'https://twitter.com/hashtag/equitabletechfuture',
+  photo: Eqtech
 }
--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -3,12 +3,8 @@ import { Base } from '../layouts/base'
 import { Column } from '../components/shared/Main'
 import { NavigationLink } from '../components/shared/NavigationLink'
 import { Card, PageContainerCentre, ProjectContainer } from '../components/shared/Containers'
-import Glasgow from '../media/events/Glasgow.jpeg'
-import Eqtech from '../media/events/Eqtech.jpeg'
-import Language from '../media/events/language-acts.png'
-import SoC from '../media/events/SoC.jpg'
-import Ada from '../media/events/ada.png'
 import { Event } from '../components/shared/Event'
+import { adaLab, soC, ideating, dojo, eqTech } from '../data/portfolio'
 
 const Community = () => {
   return (
@@ -20,57 +16,27 @@ const Community = () => {
         <PageContainerCentre>
           <ProjectContainer>
             <Card>
-              {Event({
-                title: 'Adalab',
-                paragraph: 'Mentor for students at this free bootcamp to get more women in tech',
-                date: '2019',
-                url: 'https://adalab.es/hazte-voluntario/',
-                photo: Ada
-              })}
+              {Event(adaLab)}
             </Card>
           </ProjectContainer>
           <ProjectContainer>
             <Card>
-              {Event({
-                title: 'School of Code Bootcamp',
-                paragraph: 'Mentor for students at this free bootcamp for underrepresented groups in tech',
-                date: '2018 - 2019',
-                url: 'https://schoolofcode.co.uk',
-                photo: SoC
-              })}
+              {Event(soC)}
             </Card>
           </ProjectContainer>
           <ProjectContainer>
             <Card>
-              {Event({
-                title: 'Ideating the Modern Language Curriculum',
-                paragraph: 'Organiser of a collaboration with educators on impact of tech for ML learning',
-                date: '15.06.19',
-                url: 'https://languageacts.org',
-                photo: Language
-              })}
+              {Event(ideating)}
             </Card>
           </ProjectContainer>
           <ProjectContainer>
             <Card>
-              {Event({
-                title: 'Glasgow Clojure Dojo',
-                paragraph: 'Organiser of meetup bringing together all levels to improve Clojure skills dojos',
-                date: '2018 - 2019',
-                url: 'https://www.meetup.com/Glasgow-Software-Development-Meetup/',
-                photo: Glasgow
-              })}
+              {Event(dojo)}
             </Card>
           </ProjectContainer>
           <ProjectContainer>
             <Card>
-              {Event({
-                title: 'Ideating an Equitable Tech Future',
-                paragraph: 'Organiser of a day event to explore using tech for good with charities & third sector',
-                date: '19.09.17',
-                url: 'https://twitter.com/hashtag/equitabletechfuture',
-                photo: Eqtech
-              })}
+              {Event(eqTech)}
             </Card>
           </ProjectContainer>
         </PageContainerCentre>
