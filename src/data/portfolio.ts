cd77e8c8fabef3262ca27dc76fb17ebaf6f9afb2
Charlotte Fereday <charlotte.fereday@tesglobal.com>
2020-04-12 23:28:15 +0200

    Refactors project data to be separate from component

---
+++ src/data/portfolio.ts
@@ -0,0 +1,40 @@
+import Admin from '../media/projects/Admin.png'
+import charlotteWebsite from '../media/projects/CharloteWebsite.jpg'
+import reduceRewrite from '../media/projects/ReduceRewrite.jpg'
+import flatChore from '../media/projects/FlatChores.jpg'
+import coderNewbie from '../media/projects/CoderNewbie.jpg'
+
+export const nodeProject = {
+  paragraph: 'Built using Javascript, Node, Postgres, Bcrypt, JWT ',
+  url: 'https://github.com/cfereday/pet-shop/tree/master/shop',
+  title: 'An authentication & authorisation app',
+  image: Admin
+}
+
+export const personalWebsite = {
+  paragraph: 'Built using TypeScript, React and Gatsby, a static site.',
+  url: 'https://github.com/charlottebrf/charlottebrf.dev',
+  title: 'Personal Website',
+  image: charlotteWebsite
+}
+
+export const clojureReduce = {
+  paragraph: 'A Clojure experiment in rewriting core functions to better understand them.',
+  url: 'https://github.com/charlottebrf/writing-my-own-reduce-function',
+  title: 'Rewriting core functions',
+  image: reduceRewrite
+}
+
+export const claraRules = {
+  paragraph: 'A toy project using Clojure Clara Rules to best understand Clara.',
+  url: 'https://github.com/charlottebrf/flat-chores-engine',
+  title: 'Flat Rules Engine',
+  image: flatChore
+}
+
+export const newbie = {
+  paragraph: 'An open source website with free resources to learn to code.',
+  url: 'https://github.com/charlottebrf/coder-newbie-advice',
+  title: 'Coder Newbie',
+  image: coderNewbie
+}
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react'
 import { Base } from '../layouts/base'
-import coderNewbie from '../media/projects/CoderNewbie.jpg'
-import flatChore from '../media/projects/FlatChores.jpg'
-import charlotteWebsite from '../media/projects/CharloteWebsite.jpg'
-import reduceRewrite from '../media/projects/ReduceRewrite.jpg'
-import Admin from '../media/projects/Admin.png'
 import { Column } from '../components/shared/Main'
 import { NavigationLink } from '../components/shared/NavigationLink'
 import { PageContainerCentre } from '../components/shared/Containers'
 import { Project } from '../components/shared/Project'
+import { nodeProject, personalWebsite, clojureReduce, claraRules, newbie } from '../data/portfolio'
 
 const Portfolio = () => (
   <Base>
@@ -17,36 +13,11 @@ const Portfolio = () => (
         <NavigationLink to="/">Back to Homepage</NavigationLink>
       </Column>
       <PageContainerCentre>
-        {Project({
-          paragraph: 'Built using Javascript, Node, Postgres, Bcrypt, JWT ',
-          url: 'https://github.com/cfereday/pet-shop/tree/master/shop',
-          title: 'An authentication & authorisation app',
-          image: Admin
-        })}
-        {Project({
-          paragraph: 'Built using TypeScript, React and Gatsby, a static site.',
-          url: 'https://github.com/charlottebrf/charlottebrf.dev',
-          title: 'Personal Website',
-          image: charlotteWebsite
-        })}
-        {Project({
-          paragraph: 'A Clojure experiment in rewriting core functions to better understand them.',
-          url: 'https://github.com/charlottebrf/writing-my-own-reduce-function',
-          title: 'Rewriting core functions',
-          image: reduceRewrite
-        })}
-        {Project({
-          paragraph: 'A toy project using Clojure Clara Rules to best understand Clara.',
-          url: 'https://github.com/charlottebrf/flat-chores-engine',
-          title: 'Flat Rules Engine',
-          image: flatChore
-        })}
-        {Project({
-          paragraph: 'An open source website with free resources to learn to code.',
-          url: 'https://github.com/charlottebrf/coder-newbie-advice',
-          title: 'Coder Newbie',
-          image: coderNewbie
-        })}
+        {Project(nodeProject)}
+        {Project(personalWebsite)}
+        {Project(clojureReduce)}
+        {Project(claraRules)}
+        {Project(newbie)}
       </PageContainerCentre>
     </div>
   </Base>
